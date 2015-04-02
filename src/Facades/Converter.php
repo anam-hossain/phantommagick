151242f7317383fa39df93424bd83806b1a085ae
Anam hossain <anam@hotmail.com.au>
2015-04-02 15:44:36 +1100

    ConverterServiceProvider added

---
+++ src/Facades/Converter.php
@@ -0,0 +1,9 @@
+<?php
+namespace Anam\PhantomMagick\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Converter extends Facade
+{
+    protected static function getFacadeAccessor() { return 'converter'; }
+}
