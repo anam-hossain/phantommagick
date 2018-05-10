3e16e708c683f7f341ecd7cdf1a044c36e34b7ce
klyupav <pasha.klyuchnikov@gmail.com>
2018-05-10 16:22:10 +0500

    Added page load timeout

--- src/scripts/phantom_magick.js
+++ src/scripts/phantom_magick.js
@@ -1,11 +1,11 @@
 var page = require('webpage').create(),
     system = require('system'),
-    quality = system.args[5] || '70',
-    orientation = system.args[6] || 'portrait',
-    margin = system.args[7] || '1cm',
+    quality = system.args[6] || '70',
+    orientation = system.args[7] || 'portrait',
+    margin = system.args[8] || '1cm',
     address, output, size;
 
-if (system.args.length < 3 || system.args.length > 8) {
+if (system.args.length < 4 || system.args.length > 9) {
     console.log('Usage: rasterize.js URL filename [paperwidth*paperheight|paperformat] [zoom] [orientation] [margin] [quality]');
     console.log('  paper (pdf output) examples: "5in*7.5in", "10cm*20cm", "A4", "Letter"');
     console.log('  image (png/jpg output) examples: "1920px" entire page, window width 1920px');
@@ -13,14 +13,15 @@ if (system.args.length < 3 || system.args.length > 8) {
     phantom.exit(1);
 } else {
     address = system.args[1];
-    output = system.args[2];
+    msec = system.args[2];
+    output = system.args[3];
     page.viewportSize = { width: 1280, height: 720 };
-    if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
-        size = system.args[3].split('*');
+    if (system.args.length > 4 && system.args[3].substr(-4) === ".pdf") {
+        size = system.args[4].split('*');
         page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: margin }
-                                           : { format: system.args[3], orientation: orientation, margin: margin };
-    } else if (system.args.length > 3 && system.args[3].substr(-2) === "px") {
-        size = system.args[3].split('*');
+                                           : { format: system.args[4], orientation: orientation, margin: margin };
+    } else if (system.args.length > 4 && system.args[4].substr(-2) === "px") {
+        size = system.args[4].split('*');
         if (size.length === 2) {
             pageWidth = parseInt(size[0], 10);
             pageHeight = parseInt(size[1], 10);
@@ -29,15 +30,15 @@ if (system.args.length < 3 || system.args.length > 8) {
         } else {
             // If a number received, instead of width and height,
             // Try to use best width and height by inspecting the number.
-            console.log("size:", system.args[3]);
-            pageWidth = parseInt(system.args[3], 10);
+            console.log("size:", system.args[4]);
+            pageWidth = parseInt(system.args[4], 10);
             pageHeight = parseInt(pageWidth * 3/4, 10); // it's as good an assumption as any
             console.log ("pageHeight:",pageHeight);
             page.viewportSize = { width: pageWidth, height: pageHeight };
         }
     }
-    if (system.args.length > 4) {
-        page.zoomFactor = system.args[4];
+    if (system.args.length > 5) {
+        page.zoomFactor = system.args[5];
     }
     
     // Add better error reporting when url fails to load. 
@@ -58,7 +59,7 @@ if (system.args.length < 3 || system.args.length > 8) {
             window.setTimeout(function () {
                 page.render(output, {quality: quality});
                 phantom.exit();
-            }, 200);
+            }, msec);
         }
     });
 }
